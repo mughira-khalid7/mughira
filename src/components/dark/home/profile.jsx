443f78c84df9b2b93e33a991353be57301e39919
MUGHIRA KHALID <120004623+mughira-khalid7@users.noreply.github.com>
2025-09-01 17:38:51 +0500

    first commit

---
+++ src/components/dark/home/profile.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <section id="home" className="intro-profile md-mb50">
+      <div className="row rest">
+        <div className="col-lg-4 box-img main-bg">
+          <div className="cont valign">
+            <div className="full-width">
+              <div className="img">
+                <img src="/assets/imgs/header/profile.jpg" alt="" />
+                <span className="icon">
+                  <img src="/assets/imgs/header/icon1.png" alt="" />
+                </span>
+                <span className="icon">
+                  <img src="/assets/imgs/header/icon2.png" alt="" />
+                </span>
+                <span className="icon">
+                  <img src="/assets/imgs/header/icon4.png" alt="" />
+                </span>
+              </div>
+              <div className="info text-center mt-30">
+                <h5>MUGHIRA KHALID</h5>
+                <p className="fz-13 text-u">Available For Freelance</p>
+              </div>
+              <div className="social text-center mt-20">
+                <a href="#0">
+                  <i className="fab fa-behance"></i>
+                </a>
+                <a href="#0">
+                  <i className="fab fa-github"></i>
+                </a>
+                <a href="#0">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a href="#0">
+                  <i className="fab fa-dribbble"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-8 content main-bg">
+          <h1>
+            Hello, I’m <span className="main-color">MUGHIRA KHALID</span>, Full-Stack
+            Developer and{' '}
+            <span className="bord">
+              Freelancer <i></i>
+            </span>{' '}
+            Based in Lahore, Pakistan.
+          </h1>
+          <div className="stauts mt-80">
+            <div className="d-flex align-items-center">
+              <div className="mr-40">
+                <div className="d-flex align-items-center">
+                  <h2>03</h2>
+                  <p>
+                    Years <br /> of Experance
+                  </p>
+                </div>
+              </div>
+              <div className="mr-40">
+                <div className="d-flex align-items-center">
+                  <h2>0.2k</h2>
+                  <p>
+                    Clients <br /> Worldwide
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="butn-presv">
+                  <a href="#0" className="butn butn-md butn-bord radius-5 skew">
+                    <span>Dwonload C.V</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;
