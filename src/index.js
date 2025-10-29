c4ed08038db44e0e2a8a6c8399af42e82af635cb
MUGHIRA KHALID <120004623+mughira-khalid7@users.noreply.github.com>
2025-10-29 17:01:36 +0500

    Fix Vercel deployment: Remove react-snap, add all portfolio improvements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-// Support for react-snap pre-rendering
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-} else {
-  ReactDOM.render(<App />, rootElement);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
